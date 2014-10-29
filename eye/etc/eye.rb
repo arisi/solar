ec72bd4dc65f4af148ba44e8fd48e9a9107b5d39
Kristola <kristola.ari@gmail.com>
2014-10-30 01:09:52 +0200

    nyt

--- eye/etc/eye.rb
+++ eye/etc/eye.rb
@@ -4,8 +4,8 @@ Eye.config do
   http :enable => true, :host => "0.0.0.0", :port => 12345
   logger '/var/log/eye.log'
   mail :host => "mail.voicestream.fi", :port => 25, :domain => "lnx.fi", :from_mail => "eye@lnx.fi"
-  contact :errors, :mail, 'ari.siitonen@voicestream.fi', :from_mail => "eye@lnx.fi"
-  contact :dev, :mail, 'ari.siitonen@voicestream.fi', :from_mail => "eye@lnx.fi"
+  contact :errors, :mail, 'kristola.ari@gmail.com', :from_mail => "eye@lnx.fi"
+  contact :dev, :mail, 'kristola.ari@gmail.com', :from_mail => "eye@lnx.fi"
 end
 
 Eye.load('/etc/eye/*.eye')
--- rpoll.rb
+++ rpoll.rb
@@ -9,32 +9,32 @@ $temp=0
 
 $redis = Redis.new(:host => 'localhost' )
 
-$mode=ARGV[0]||="mittaus"
-puts "rpoll starting in mode #{$mode}"
+#$mode=ARGV[0]||="sim"
+#puts "rpoll starting in mode #{$mode}"
 
 def stamp
   return DateTime.now.strftime.gsub(/\D/,'')[0..11]
 end
 
-if $mode!='sim'
-  require 'bcm2835'
-  include Bcm2835
-end
+#if $mode!='sim'
+#  require 'bcm2835'
+#  include Bcm2835
+#end
 
 loop do
   sleep 1
-  if $mode=='sim'
-    $temp=40+Random.rand(10)
-    $kwh=2.0+Random.rand(10)/10.0
-  else
-    pin = 17
-    GPIO.output(pin)
-    SPI.begin do |spi| 
-      $spi=spi.read       # returns 1 byte
+#  if $mode=='sim'
+#    $temp=40+Random.rand(10)
+#    $kwh=2.0+Random.rand(10)/10.0
+#  else
+#    pin = 17
+#    GPIO.output(pin)
+#    SPI.begin do |spi| 
+#      $spi=spi.read       # returns 1 byte
       #puts spi.read(1) # returns an array of 1024 bytes 
-    end
-    puts "poll: #{$tick} #{$spi}"
-  end
+#    end
+#    puts "poll: #{$tick} #{$spi}"
+#  end
   ss=stamp
   puts "loop #{$tick} #{ss}"
   
